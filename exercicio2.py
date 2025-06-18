c97489abfc81db3a919e851a54cff79e45e9680d
GustavoLinhares <99147725+GustavoLinharess@users.noreply.github.com>
2025-06-18 17:56:11 -0300

    Delete exercicio2.py

--- exercicio2.py
+++
@@ -1,8 +0,0 @@
-idade = int(input('Fale sua idade:'))
-
-if idade <= 12:
-  print(f"Você é uma criança")
-elif idade  > 12 and idade <= 18:
-  print(f"Você é um adolescente")
-elif idade  > 18:
-   print (f"Você é um adulto!!")  
